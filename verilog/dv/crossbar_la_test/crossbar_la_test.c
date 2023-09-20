7d4f5aa0141dbd19e222fcc8879934801cbd478f
Aiden Petersen <aidenp@iastate.edu>
2023-09-20 15:47:41 -0500

    Updated driver files

--- verilog/dv/crossbar_la_test/crossbar_la_test.c
+++ verilog/dv/crossbar_la_test/crossbar_la_test.c
@@ -1,5 +1,52 @@
-#include <defs.h>
-#include <stub.c>
+#include "verilog/dv/caravel/defs.h"
+// #include <stub.c>
+#define WAIT_ITERATIONS 10
+
+
+void write(uint8_t value, uint8_t line){
+  uint32_t op;
+  // la[ 7: 0]
+  uint32_t bitline    = value;
+  // la[15: 8]
+  uint32_t selectline = (~value) << 8;
+  // la[23:16]
+  uint32_t wordline   = (1 << line) << 16;
+  op = bitline | selectline | line_onehot;
+  reg_la0_data = op;
+  for(int i = 0; i < WAIT_ITERATIONS; i++){
+    __asm("nop");
+  }
+  reg_la0_data = 0;
+}
+
+uint8_t read(uint8_t line){
+  uint32_t op;
+  uint32_t wordline = (1 << line) << 16;
+  op = wordline;
+  reg_la0_data = op;
+  for(int i = 0; i < WAIT_ITERATIONS; i++){
+    __asm("nop");
+  }
+  uint8_t result = (uint8_t) reg_la1_data;
+  reg_la0_data = 0;
+  return result;
+}
+
+uint8_t mac(uint8_t value){
+  uint32_t op;
+  uint32_t selectline = value;
+  uint32_t bitline    = (~value) << 8;
+  uint32_t wordline   = (0xFF) << 16;
+
+  op = selectline | bitline | wordline;
+  reg_la0_data = op;
+  for(int i = 0; i < WAIT_ITERATIONS; i++){
+    __asm("nop");
+  }
+  uint8_t result = (uint8_t) reg_la1_data;
+  reg_la0_data = 0;
+  return result;
+}
 
 void main(){
   // setup GPIO  config
@@ -57,44 +104,30 @@ void main(){
    * la3
    * 96:127 : b
    */
-  // x stuff
-  reg_la0_oenb = reg_la0_iena = 0x000000FF;
+  // Enable inputs
+  reg_la0_oenb = reg_la0_iena = 0xFFFFFFFF;
 
-  // A stuff
-  reg_la1_oenb = reg_la0_iena = 0x0001FFFF;
+  // Enable Outputs
+  reg_la1_oenb = reg_la0_iena = 0x00000000;
 
   // doesn't matter
   reg_la2_oenb = reg_la0_iena = 0x00000000;
 
-  // b stuff
+  // doesn't matter
   reg_la3_oenb = reg_la3_iena = 0x00000000;
 
-  // Write identity matrix weights
-  for(int i = 0; i < 8; i++){
-    // set write row
-    reg_la1_data &= 0xFFFFFF00;
-    reg_la1_data |= (1 << (8-i));
+  // Write identity matrix
+  write(0x01, 1);
+  write(0x02, 2);
+  write(0x04, 3);
+  write(0x08, 4);
+  write(0x10, 5);
+  write(0x20, 6);
+  write(0x40, 7);
+  write(0x80, 8);
 
-    // set write data
-    reg_la1_data &= 0xFFFF00FF;
-    reg_la1_data |= (1 << (i + 8));
+  int r = read(3);
 
-    // start write
-    reg_la1_data |= 0x00010000;
-    // wait for write to complete
-   
-    // clear just the 16th bit
-    reg_la1_data &= 0xFFFEFFFF;
-  }
-
-  // assign x val
-  reg_la0_data |= 0b10110101;
-  // printf("%d", reg_la3_data);
-
-  reg_la0_data |= 0b10100010;
-  // printf("%d", reg_la3_data);
-
-  reg_la0_data |= 0b11110100;
-  // printf("%d", reg_la3_data);
+  int m = mac(0xA5);
 
 }
